86eece07f0368c0901f6d0966d02f648fa6da489
Renato Costa <renatoaccosta@gmail.com>
2022-03-16 14:49:57 +0000

    Iteration 1 Done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User.model");
+const MongoStore = require("connect-mongo");
 
-/* GET home page */
+const saltRounds = 10;
+// GET home page
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+//GET signup page
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+// POST signup page
+router.post("/signup", async (req, res, next) => {
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(req.body.password, salt);
+  const newUser = {
+    username: req.body.username,
+    password: hash,
+  };
+  await User.create(newUser);
+  res.render("profile");
+});
+
+//GET login page
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+//POST login page
+router.post("/login", async (req, res, next) => {});
 
 module.exports = router;
